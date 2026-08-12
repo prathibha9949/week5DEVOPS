9b581447be8d36a6c9dec635465d033ec72b5bd6
prathibha9949 <samaprathibha7@gmail.com>
2026-08-12 10:08:37 +0530

    Add test for Factorial calculation

--- TestFactorial.java
+++ TestFactorial.java
@@ -1,1 +1,20 @@
+public class TestFactorial {
 
+    public static void main(String[] args) {
+
+        Factorial f = new Factorial();
+
+        long expected = 120;
+        long actual = f.calculate(5);
+
+        if (actual == expected) {
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: " + expected);
+            System.out.println("Actual: " + actual);
+
+            System.exit(1);   // Tell Jenkins that test failed
+        }
+    }
+}
